7211abdedc2b4e3200d7b3360144dd92bd40840a
muyao <muyao1987@qq.com>
2020-02-17 17:00:14 +0800

    修改汉化处理

--- js/index.js
+++ js/index.js
@@ -63,6 +63,8 @@ function initMap(Cesium) {
                 //sceneMode: Cesium.SceneMode.SCENE2D,  
             });
 
+            //汉化原生cesium
+            mars3d.loadCesiumZH();
 
             //如果有xyz传参，进行定位 
             if (haoutil.isutil.isNotNull(request.x)
@@ -188,7 +190,7 @@ function initWork(viewer) {
 
 
     bindShowTilesParts();
-    navigationHelpButtonCHE()
+  
 }
 
 
@@ -302,59 +304,4 @@ function activateFunByMenu(fun) {
 function goHome() {
     mars3d.widget.disableAll()
     viewer.mars.centerAt();
-}
-
-
-
-
-//
-function navigationHelpButtonCHE() {
-    //汉化帮助信息
-    if (viewer.sceneModePicker) {
-        viewer.sceneModePicker.viewModel.tooltip3D = "三维";
-        viewer.sceneModePicker.viewModel.tooltip2D = "二维";
-        viewer.sceneModePicker.viewModel.tooltipColumbusView = "哥伦布视图";
-    }
-    if (viewer.navigationHelpButton) {
-        viewer.navigationHelpButton.viewModel.tooltip = "操作指南";
-        var clickHelper = viewer.navigationHelpButton.container.getElementsByClassName("cesium-click-navigation-help")[0];
-        var touchHelper = viewer.navigationHelpButton.container.getElementsByClassName("cesium-touch-navigation-help")[0];
-
-        var button = viewer.navigationHelpButton.container.getElementsByClassName("cesium-navigation-button-right")[0];
-        button.innerHTML = button.innerHTML.replace(">Touch", ">触摸");
-        button = viewer.navigationHelpButton.container.getElementsByClassName("cesium-navigation-button-left")[0];
-        button.innerHTML = button.innerHTML.replace(">Mouse", ">鼠标");
-
-        var click_help_pan = clickHelper.getElementsByClassName("cesium-navigation-help-pan")[0];
-        click_help_pan.innerHTML = "平移";
-        var click_help_pan_details = click_help_pan.parentNode.getElementsByClassName("cesium-navigation-help-details")[0];
-        click_help_pan_details.innerHTML = "按下左键 + 拖动";
-
-        var click_help_zoom = clickHelper.getElementsByClassName("cesium-navigation-help-zoom")[0];
-        click_help_zoom.innerHTML = "旋转";
-        click_help_zoom.parentNode.getElementsByClassName("cesium-navigation-help-details")[0].innerHTML = "按下右键+拖动";
-        click_help_zoom.parentNode.getElementsByClassName("cesium-navigation-help-details")[1].innerHTML = "";
-
-        var click_help_rotate = clickHelper.getElementsByClassName("cesium-navigation-help-rotate")[0];
-        click_help_rotate.innerHTML = "缩放";
-        click_help_rotate.parentNode.getElementsByClassName("cesium-navigation-help-details")[0].innerHTML = "滚动鼠标滚轮";
-        click_help_rotate.parentNode.getElementsByClassName("cesium-navigation-help-details")[1].innerHTML = "";
-        //触屏操作
-        var touch_help_pan = touchHelper.getElementsByClassName("cesium-navigation-help-pan")[0];
-        touch_help_pan.innerHTML = "平移";
-        touch_help_pan.parentNode.getElementsByClassName("cesium-navigation-help-details")[0].innerHTML = "单指拖动";
-
-        var touch_help_zoom = touchHelper.getElementsByClassName("cesium-navigation-help-zoom")[0];
-        touch_help_zoom.innerHTML = "缩放";
-        touch_help_zoom.parentNode.getElementsByClassName("cesium-navigation-help-details")[0].innerHTML = "双指捏合";
-
-        var touch_help_tilt = touchHelper.getElementsByClassName("cesium-navigation-help-rotate")[0];
-        touch_help_tilt.innerHTML = "俯仰";
-        touch_help_tilt.parentNode.getElementsByClassName("cesium-navigation-help-details")[0].innerHTML = "双指同向拖动";
-
-        var touch_help_rotate = touchHelper.getElementsByClassName("cesium-navigation-help-tilt")[0];
-        touch_help_rotate.innerHTML = "旋转";
-        touch_help_rotate.parentNode.getElementsByClassName("cesium-navigation-help-details")[0].innerHTML = "双指反向拖动";
-    }
-
-}
+} 
